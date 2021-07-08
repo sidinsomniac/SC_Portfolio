91117b4237f5d02fa9c2492bb76617e7ab99f07f
sidinsomniac <chatterjeesid1993@gmail.com>
2021-07-08 17:06:43 +0530

    Add tech cube

--- assets/data/project-data.js
+++ assets/data/project-data.js
@@ -2,91 +2,91 @@ export default [
     {
         title: "Yelp Camp",
         description: "A simple MEAN stack app to share favourite campgrounds",
-        imgUrl: "./images/projects/yelpcamp.jpg",
+        imgUrl: "./assets/images/projects/yelpcamp.jpg",
         url: "https://fast-tor-50255.herokuapp.com/"
     },
     {
         title: "Restaurant Review App",
         description: "A mobile-ready responsive, progressive and accessible web application to search various",
-        imgUrl: "./images/projects/restaurant.jpg",
+        imgUrl: "./assets/images/projects/restaurant.jpg",
         url: "https://sidinsomniac.github.io/restaurant-review-app/"
     },
     {
         title: "Neighborhood Map",
         description: "An SPA made using React, displaying a map populated with many markers depicting neighbourhood",
-        imgUrl: "./images/projects/neighborhood.jpg",
+        imgUrl: "./assets/images/projects/neighborhood.jpg",
         url: "https://sidinsomniac.github.io/Neigbourhood_Map/"
     },
     {
         title: "Matching Game",
         description: "A matching game which vigorously tests your memory skills",
-        imgUrl: "./images/projects/memory.jpg",
+        imgUrl: "./assets/images/projects/memory.jpg",
         url: "https://sidinsomniac.github.io/matching-game/"
     },
     {
         title: "Classic Frogger",
         description: "A classic arcade game which tests your reflexes in the real time",
-        imgUrl: "./images/projects/frogger.jpg",
+        imgUrl: "./assets/images/projects/frogger.jpg",
         url: "https://sidinsomniac.github.io/Classic-Arcade-Frogger/"
     },
     {
         title: "PixArt",
         description: "A simple pixel art maker heavily based on events, and flaunts the 90's era theme, that tests",
-        imgUrl: "./images/projects/pixel.jpg",
+        imgUrl: "./assets/images/projects/pixel.jpg",
         url: "https://sidinsomniac.github.io/Pixel-Art-Maker/"
     },
     {
         title: "Reaction Tester",
         description: "Another game which is meant to test your reflexes and reaction timing",
-        imgUrl: "./images/projects/reaction.jpg",
+        imgUrl: "./assets/images/projects/reaction.jpg",
         url: "https://sidinsomniac.github.io/ReactionTester-Plus/"
     },
     {
         title: "Typing Test",
         description: "A simple application which targets your typing and accuracy skills in real time",
-        imgUrl: "./images/projects/typing.jpg",
+        imgUrl: "./assets/images/projects/typing.jpg",
         url: "https://sidinsomniac.github.io/typingtest/"
     },
     {
         title: "Code Editor",
         description: "Made by coding, and intended for the purpose of coding to test your coding skills",
-        imgUrl: "./images/projects/code.jpg",
+        imgUrl: "./assets/images/projects/code.jpg",
         url: "https://sidinsomniac.github.io/Code-PlayGround/"
     },
     {
         title: "Log Keeper",
         description: "A simple log keeper where you can add or remove items for any simple tasks (hint: grocery)",
-        imgUrl: "./images/projects/logkeep.jpg",
+        imgUrl: "./assets/images/projects/logkeep.jpg",
         url: "https://sidinsomniac.github.io/Log-Keeper/"
     },
     {
         title: "Fingerdrum",
         description: "If you are a musician, you will love this simple percussion instrument that tests your rhythm",
-        imgUrl: "./images/projects/finger.jpg",
+        imgUrl: "./assets/images/projects/finger.jpg",
         url: "https://sidinsomniac.github.io/Finger-Drum/"
     },
     {
         title: "Color Guessing",
         description: "A simple JS based game that tests your knowledge on how the RGB system works in",
-        imgUrl: "./images/projects/rgb.jpg",
+        imgUrl: "./assets/images/projects/rgb.jpg",
         url: "https://codepen.io/sidInsomniac/full/ypRZME/"
     },
     {
         title: "Digital Clock",
         description: "A digital clock made using pure CSS and JS",
-        imgUrl: "./images/projects/digital.jpg",
+        imgUrl: "./assets/images/projects/digital.jpg",
         url: "https://sidinsomniac.github.io/digi_clock/"
     },
     {
         title: "Analog Clock",
         description: "A beautiful SVG, CSS and JS based Grandfather Clock",
-        imgUrl: "./images/projects/analog.jpg",
+        imgUrl: "./assets/images/projects/analog.jpg",
         url: "https://sidinsomniac.github.io/analog_clock/"
     },
     {
         title: "Patatap Clone",
         description: "A simple recreation of the website Patatap, using PaperJS, HowlerJS and Canvas",
-        imgUrl: "./images/projects/patatap.jpg",
+        imgUrl: "./assets/images/projects/patatap.jpg",
         url: "https://sidinsomniac.github.io/patatap_clone/"
     }
 ]
